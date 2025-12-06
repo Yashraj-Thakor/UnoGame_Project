81e5b12ab93fbe98c7221eae8624f8ab2c495e75
Yashraj-Thakor <thayashr@sheridancollege.ca>
2025-12-05 23:21:15 -0500

    Uno game Updated

--- src/ca/sheridancollege/project/UnoGame.java
+++ src/ca/sheridancollege/project/UnoGame.java
@@ -1,42 +1,92 @@
-//Yashraj
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ca.sheridancollege.project;
 
-/**
- *
- * @author HP
- */
 import java.util.ArrayList;
 
 public class UnoGame extends Game {
-
     private Deck deck;
     private ArrayList<UnoCard> discardPile = new ArrayList<>();
 
     public UnoGame(String name, ArrayList<Player> players) {
         super(name);
-        this.deck = new Deck();
         this.setPlayers(players);
-    }
 
-    public Deck getDeck() {
-        return deck;
-    }
-
-    public ArrayList<UnoCard> getDiscardPile() {
-        return discardPile;
+        deck = new Deck();
     }
 
     @Override
     public void play() {
-        // Implement during Deliverable 3
+        System.out.println("UNO GAME STARTED!");
+        deck.shuffle();
+
+        // Deal 7 cards to each player
+        for (Player p : getPlayers()) {
+            UnoPlayer up = (UnoPlayer) p;
+            for (int i = 0; i < 7; i++) {
+                up.getHand().getCards().add((UnoCard) deck.getCards().remove(0));
+            }
+        }
+
+       
+        UnoCard topCard = (UnoCard) deck.getCards().remove(0);
+        discardPile.add(topCard);
+        System.out.println("Starting card: " + topCard);
+
+        boolean gameOver = false;
+
+        while (!gameOver) {
+            for (Player p : getPlayers()) {
+                UnoPlayer up = (UnoPlayer) p;
+                System.out.println("\n" + up.getName() + "'s turn. Top card: " + topCard);
+
+                UnoCard played = up.playCard(topCard);
+
+                if (played != null) {
+                    discardPile.add(played);
+                    topCard = played;
+                    System.out.println(up.getName() + " played: " + played);
+
+               
+                    if (up.getHand().getCards().isEmpty()) {
+                        declareWinner(up);
+                        gameOver = true;
+                        break;
+                    }
+
+                } else {
+                  
+                    System.out.println(up.getName() + " had no match. Drew a card.");
+                    up.drawCard(deck);
+                }
+
+               
+                if (deck.getCards().isEmpty()) {
+                    System.out.println("Deck empty! No winner found.");
+                    gameOver = true;
+                    break;
+                }
+
+             
+                try {
+                    Thread.sleep(600);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
     }
 
     @Override
     public void declareWinner() {
-        // Implement during Deliverable 3
+        System.out.println("Game Over! No winner this time.");
+    }
+
+    public void declareWinner(Player winner) {
+        System.out.println("\n------------------------------------------");
+        System.out.println("Congratulations, " + winner.getName() + " wins the UNO game!");
+        System.out.println("------------------------------------------\n");
     }
 }
