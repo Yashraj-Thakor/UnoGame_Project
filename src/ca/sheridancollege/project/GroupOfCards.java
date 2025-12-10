e52d90e33c6af7009b6bea27dc6d2171df10da85
Yashraj-Thakor <thayashr@sheridancollege.ca>
2025-12-10 12:09:31 -0500

    Uno game Updated

--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -1,4 +1,4 @@
-// Updated for UNO Game Project
+
 /**
  * SYST 17796 Project Base code.
  * Students can modify and extend to implement their game.
@@ -9,48 +9,35 @@ package ca.sheridancollege.project;
 import java.util.ArrayList;
 import java.util.Collections;
 
-/**
- * A concrete class that represents any grouping of cards for a Game. HINT, you might want to subclass this more than
- * once. The group of cards has a maximum size attribute which is flexible for reuse.
- *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
- */
 public class GroupOfCards {
 
-    //The group of cards, stored in an ArrayList
+  
     private ArrayList<Card> cards;
-    private int size;//the size of the grouping
+    private int size;
 
     public GroupOfCards(int size) {
         this.size = size;
+        this.cards = new ArrayList<>();  
     }
 
-    /**
-     * A method that will get the group of cards as an ArrayList
-     *
-     * @return the group of cards.
-     */
     public ArrayList<Card> getCards() {
         return cards;
+    }
+
+    public void setCards(ArrayList<Card> cards) {  
+        this.cards = cards;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
     }
 
     public void shuffle() {
         Collections.shuffle(cards);
     }
-
-    /**
-     * @return the size of the group of cards
-     */
-    public int getSize() {
-        return size;
-    }
-
-    /**
-     * @param size the max size for the group of cards
-     */
-    public void setSize(int size) {
-        this.size = size;
-    }
-
-}//end class
+}
+//end class
